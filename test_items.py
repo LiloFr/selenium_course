36ca3a4fa39b338959d7f9bd27e9f1aab061de98
LiloFr <88384549+LiloFr@users.noreply.github.com>
2022-03-27 16:50:09 +0300

    true assert

--- test_items.py
+++ test_items.py
@@ -2,5 +2,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_site_have_button(browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector(".btn-primary")
-    assert button, "Can't find the button!"
+    button = len(browser.find_elements_by_css_selector(".btn-primary"))
+    assert button > 0, "Can't find the button!"
